48e73193557b4e753772b21d95da488aedaac8e6
Tobbala <tobbala@emea.corpdir.net>
2025-07-22 20:42:55 +0200

    using google as host for videos

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingScreen.css"; // optional animation styles
+
+const LoadingScreen = () => {
+  return (
+    <div className="loading-screen">
+      <div className="loader">Loading...</div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
